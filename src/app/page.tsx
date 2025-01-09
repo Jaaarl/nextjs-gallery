4ea4975420a128bef83584ff0804c509b52fade5
Jarl <jarl13.work@gmail.com>
2025-01-09 14:58:58 +0800

    modify: order image by descending order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany();
-  console.log(images);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
         {images.map((image) => (
-          <div key={image.id} className={"w-48 flex flex-col gap-1"}>
+          <div key={image.id} className={"flex w-48 flex-col gap-1"}>
             <img src={image.url} />
             {image.name}
           </div>
